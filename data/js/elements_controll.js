acf3002ce57abb98ec9f9d8ca3f9812c66a6565f
Mikhail-Semerkov <mikhail.semerkov@mail.ru>
2022-01-24 22:59:39 +0300

    password play

--- data/js/elements_controll.js
+++ data/js/elements_controll.js
@@ -110,3 +110,17 @@ function home_click()
 {
   document.location.reload();
 }
+
+
+
+function show_hide_password(target){
+	var input = document.getElementById('settings_wifi_pass');
+	if (input.getAttribute('type') == 'password') {
+		target.classList.add('view');
+		input.setAttribute('type', 'text');
+	} else {
+		target.classList.remove('view');
+		input.setAttribute('type', 'password');
+	}
+	return false;
+}
